a1d02bb71e5b8136e96b1ee0cca9124dc949555d
PrincipX <mucholimited@protonmail.com>
2022-07-18 16:32:53 +0200

    Added Flag.java, which stores flag of country

---
+++ weatherNow/src/core/Flag.java
@@ -0,0 +1,28 @@
+package core;
+
+import java.awt.image.BufferedImage;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
+public class Flag {
+	public ImageIcon image;
+	
+	public Flag(String country) {
+		this.image = getFlag(country);
+	}
+	
+	private ImageIcon getFlag(String country) {
+		country.replaceAll(" ", "%20");
+		URL url;
+		try {
+			url = new URL("https://countryflagsapi.com/png/"+country);
+			BufferedImage c = ImageIO.read(url);
+			return new ImageIcon(c);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+}
